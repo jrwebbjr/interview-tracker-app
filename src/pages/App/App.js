86f2c4d915dc703823bc0db5b7a8ea4e7886c3e0
jrwebbjr <webbjr@zohomail.com>
2022-04-19 19:43:10 -0500

    Added skeleton of pages as well as form component, building out form in process

---
+++ src/pages/App/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Home from ("../Home/Home");
+import New from ("../New/New");
+import Show from ("../Show/Show");
+import Edit from ("../Edit/Edit");
+
+function App() {
+  return (
+    <main className="App">
+      <Home />
+      <New />
+      <Show />
+      <Edit />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Show/Show.js
@@ -0,0 +1,5 @@
+export default function Show(){
+    return (
+        <h1>Show Page</h1>
+    )
+}
