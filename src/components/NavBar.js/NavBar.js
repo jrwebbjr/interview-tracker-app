feface3d3c50be257639573de801e19fff5b3293
jrwebbjr <webbjr@zohomail.com>
2022-04-20 20:26:56 -0500

    added logo to navbar

--- src/components/NavBar.js/NavBar.js
+++ src/components/NavBar.js/NavBar.js
@@ -2,13 +2,11 @@ import SearchBar from "../SearchBar/SearchBar";
 import { TiHome } from "react-icons/ti";
 import { TiDocumentAdd } from "react-icons/ti";
 import { TiUser } from "react-icons/ti";
-import Logo from "../../../public/Logo.jpg";
 
 export default function NavBar(){
     return(
         <div className="navbar">
             <h1 className="logo">App-ly</h1>
-            <img src={Logo} alt="Logo" />
                 <ul className="nav-list">
                     <li className="nav-links"><TiHome className="icon" /> Home</li>
                     <li className="nav-links"><TiDocumentAdd className="icon" /> Add New Job</li>
