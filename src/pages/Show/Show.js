78c4461e938e10e7bdfe4d7f5cf9eaf6d090851b
jrwebbjr <webbjr@zohomail.com>
2022-04-26 20:41:46 -0500

    started building the job show page felt I needed to bring in tailwind before I geet much further. Installed and configured tailwind

--- src/pages/Show/Show.js
+++ src/pages/Show/Show.js
@@ -1,13 +1,29 @@
-import DefaultLayout from "../layout/DefaultLayout";
+// import DefaultLayout from "../layout/DefaultLayout";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Show(){
-    return (
-        <div>
-            <DefaultLayout />
-            <h1>Show Page</h1>
-        </div>
-        
-    )
+export default function Jobs(){
+    const [jobs, setJobs] = useState([])
+
+    const getAllJobs = async() => {
+        const res = await axios.get('/api/jobs')
+        setJobs(res.data)
+    }
+
+    useEffect(() => {
+        getAllJobs()
+    }, []) 
+
+    return(
+        <>
+            <ul>
+                {
+                    jobs.map((job) => {
+                        return <li key={job._id}>{job.company}</li> 
+                    })
+                }         
+            </ul>
+        </>
+        )
 }
-
 //TODO: Show selected job and have an update button to take to edit route for updates
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,12 +1,11 @@
-import NavBar from "../../components/navBar.js/NavBar";
 
-export default function Home(){
+export default function Landing(){
     return(
         <div>
-            <h1>Home Page</h1>
+            <h1>Landing Page</h1>
         </div>
         
     )
 }
 
-//TODO: Landing page, will ask for user to login, if you have time add photos showing what app can do on page for new users that haven't created a user name and password yet
+//TODO: User Landing page, will ask for user to login, if you have time add photos showing what app can do on page for new users that haven't created a user name and password yet
