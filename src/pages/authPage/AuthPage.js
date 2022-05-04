7fdcdd2da16b4725cacf2ad3e335c826ba848059
jrwebbjr <webbjr@zohomail.com>
2022-05-03 21:46:56 -0500

    started working on Auth, created AuthPage

---
+++ src/pages/authPage/AuthPage.js
@@ -0,0 +1,10 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+
+export default function AuthPage(props){
+    return (
+        <main>
+            <h1>Auth Page</h1>
+            <SignUpForm />
+        </main>
+    )
+}
