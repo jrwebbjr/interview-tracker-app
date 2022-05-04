a448a6b13c17537f6f13349d38b34bba7fb4d6b0
jrwebbjr <webbjr@zohomail.com>
2022-05-03 21:58:56 -0500

    added a sign up form component

---
+++ src/components/signUpForm/SignUpForm.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import { signUp } from '../../utilities/users-service';
+
+export default class SignUpForm extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: '',
+    }
+
+    handleChange = (evt) => {
+        this.setState({...this.state, [evt.target.name]: evt.target.value, error: ''})
+    }
+
+    handleSubmit = async (evt) => {
+        evt.preventDefault()
+        try{
+            const formData = {...this.state};
+            delete formData.error;
+            delete formData.confirm;
+            const user = await signUp(formData)
+            localStorage.setItem('token', user)
+        } catch (error) {
+            this.setState({ error: 'Sign Up Failed' })
+        }
+    }
+
+    render() {
+        const disable = this.state.password !== this.state.confirm;
+        return (
+            <div>
+                <div className="">
+                    <form autoComplete="off" onSubmit={this.handleSubmit}>
+                    <label>Name </label>
+                    <input className="" type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+                    <br/>
+                    <label>Email </label>
+                    <input className="" type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                    <br/>
+                    <label>Password </label>
+                    <input className="" type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                    <br/>
+                    <label>Confirm </label>
+                    <input className="" type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                    <br/>
+                    <button className="button" type="submit" disabled={disable}>SIGN UP</button>
+                    </form>
+                </div>
+                <h2 className="">&nbsp;{this.state.error}</h2>
+            </div>
+        )
+    }
+}
